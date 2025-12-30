200ec901ba63c7e02513d21b4641721ee1d82b72
Nichole Kernreicht <nicholekernreicht@gmail.com>
2025-12-29 18:17:41 -0600

    updated to not generate centered output

--- src/magwell/core.clj
+++ src/magwell/core.clj
@@ -3,16 +3,23 @@
             [scad-clj.scad  :as s]
             [clojure.java.io :as io]))
 
+
+
 (defn write-scad! [path model]
   (io/make-parents path)
   (spit path (s/write-scad model))
   path)
 
-(defn magwell-block []
-  ;; 1 unit = 1 mm
-  (m/cube 60 40 80))
+;; Units: mm (convention)
+(def dims
+  {:height 38
+   :width  90
+   :depth  65})
+
+(defn envelope []
+  (let [{:keys [width depth height]} dims]
+    (m/cube width depth height)))
 
 (defn -main [& _]
-  (write-scad! "out/magwell.scad" (magwell-block)))
-
-;; Testing magit
+  (binding [scad-clj.model/*center* false]
+    (write-scad! "out/magwell.scad" (envelope))))
