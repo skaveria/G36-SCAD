7812a6704e34f1bab0501726bb20d89049bb676c
Nichole Kernreicht <nicholekernreicht@gmail.com>
2025-12-29 18:58:37 -0600

    added bottom cut to magwell

--- src/magwell/core.clj
+++ src/magwell/core.clj
@@ -3,23 +3,30 @@
             [scad-clj.scad  :as s]
             [clojure.java.io :as io]))
 
-
-
 (defn write-scad! [path model]
   (io/make-parents path)
   (spit path (s/write-scad model))
   path)
 
-;; Units: mm (convention)
-(def dims
-  {:height 38
-   :width  90
-   :depth  65})
+(def dims {:x 90 :y 65 :z 38})
 
-(defn envelope []
-  (let [{:keys [width depth height]} dims]
-    (m/cube width depth height)))
+(defn block []
+  (let [{:keys [x y z]} dims]
+    (m/cube x y z :center false)))
+
+(defn cutter []
+  (m/translate [0 0 -1]
+      (m/extrude-linear
+       {:height 200}           ;; extrude through Y
+       (m/polygon
+        [[-1   -1]               ;; bottom-left
+         [75  -1]               ;; bottom-right
+         [75  0]              ;; top-right
+         [-1  20]]))))        ;; top-left
+
+(defn model []
+  (m/difference (block) (cutter)))
 
 (defn -main [& _]
   (binding [scad-clj.model/*center* false]
-    (write-scad! "out/magwell.scad" (envelope))))
+    (write-scad! "out/magwell.scad" (model))))
